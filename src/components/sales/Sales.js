768fd4c3b79e71fe8f14a89ecfa7d8aaca7441d8
Evandro Capovilla <vandinhocapovilla@live.com>
2019-08-03 19:37:54 -0300

    att sales

--- src/components/sales/Sales.js
+++ src/components/sales/Sales.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Slider from '../index/slider/Slider'
-import Cards from '../index/cards/Cards';
+
 // import image from '../../assets/img/home/footer-bg.jpg'
 
 export default class Sales extends React.Component {
